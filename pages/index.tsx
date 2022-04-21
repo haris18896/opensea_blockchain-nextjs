4897767b386991297a99701cd423286e2dc88904
haris96 <haris@fit-outfit.com>
2022-04-21 11:31:51 +0500

    hero added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next'
 import Header from '../components/Header/Header'
+import Hero from '../components/Hero'
 
 const Home: NextPage = () => {
   return (
     <>
      <Header />
+     <Hero />
     </>
   )
 }
